3bba7eceaba356f94b732c9a2213f079f07e09e9
Rulo <andres@rulocode.com>
2021-10-01 19:07:38 -0500

    feat: basic config
    Update README.md

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Document, { Html, NextScript, Main, Head } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+        </Head>
+        <body>
+          <Main />
+          <div id="modal-root" />
+
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+   
+   
+    <>
+    <Head>
+      <title>Next.js 100</title>
+      <meta name="Description" content="Build a PWA with Next.js to achieve 100 lighthouse score."></meta>
+    </Head>
+    <div>
+      Next.js 100
+    </div>
+  </>
+  )
+}
+
+export default Home
